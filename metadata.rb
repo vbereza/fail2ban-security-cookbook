e00915fac30e1a3f034434090dfbbb28730470ac
Valerii Bereza <661645@gmail.com>
2013-07-08 11:15:35 +0300

    change metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,7 @@ description      'Installs/Configures fail2ban-security'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 	 'fail2ban'
+
+%w{ ubuntu debian redhat centos fedora oracle suse }.each do |os|
+  supports os
+end
